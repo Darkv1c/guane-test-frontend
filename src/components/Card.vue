66031a01065a6665a2f357e8d91697d813bf7573
Darkv1c <74632476+Darkv1c@users.noreply.github.com>
2021-10-30 23:07:12 -0500

    rotation animation

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,16 +1,29 @@
 <template lang="">
-    <div class="card-container c-pointer" :style="`background-image: url('${object.image}')`">
-        <span>Name: {{object.name}}</span>
-        <span> </span>
-        <span></span>
-        <span></span>
+    <div class="card-container c-pointer" :style="`background-image: url('${object.image}')`" 
+        @mouseover="()=>setShowText(true)" @mouseleave="()=>setShowText(false)">
+        <div v-if="showText" class="card-text-container">
+            <span>Name: {{object.name}}</span>
+            <span> </span>
+            <span></span>
+            <span></span>
+        </div>
     </div>
 </template>
 <script>
 export default {
+    data() {
+        return {
+            showText: false
+        }
+    },
     props: {
         /** info to show in the cart */
         object: Object
+    },
+    methods: {
+        setShowText(show) {
+            setTimeout(() => { this.showText = show }, 250);            
+        }
     }
 }
 </script>
@@ -23,5 +36,16 @@ export default {
         margin: 12px;
         aspect-ratio: 1/1;
         flex: 0 1 25%;
+        transition: all .5s ease-in-out 0s;
+        &:hover {
+            transform: rotatey(180deg);
+            transition: all .5s ease-in-out 0s;
+        }
+        .card-text-container{
+            background-color: rgba(antiquewhite, .5);
+            min-width: 100%;
+            min-height: 100%;
+            border-radius: 40px;
+        }
     }
 </style>
